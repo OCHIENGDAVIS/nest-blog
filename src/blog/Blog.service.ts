48f0a460f31e181203b089d4e2617ecaabb38137
davis <davisochieng4@gmail.com>
2022-08-18 04:14:56 -0400

    Add Blog get all and search functionality

--- src/blog/Blog.service.ts
+++ src/blog/Blog.service.ts
@@ -1,14 +1,19 @@
 import { HttpStatus, HttpException, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { Repository, Like } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Blog } from './blog.entity';
 import { CreateBlogDto } from './dtos/create-blog.dto';
 import { UpdateBlogDto } from './dtos/update-blog.dto';
+import { BlogSearchQuery } from './dtos/blog-search-query.dto';
 
 @Injectable()
 export class BlogService {
   constructor(@InjectRepository(Blog) private blogRepo: Repository<Blog>) {}
+
+  async getAllBlogs() {
+    return await this.blogRepo.find();
+  }
 
   async createBlog(body: CreateBlogDto) {
     const blog = this.blogRepo.create(body);
@@ -35,7 +40,24 @@ export class BlogService {
     Object.assign(blog, body);
     return await this.blogRepo.save(blog);
   }
-  deleteBlog(id: number) {}
+  async deleteBlog(id: number) {
+    const blog = await this.blogRepo.findOne({ where: { id } });
+    if (!blog) {
+      throw new HttpException('blog not found', HttpStatus.NOT_FOUND);
+    }
+    await this.blogRepo.remove(blog);
+    return;
+  }
 
-  searchBlog() {}
+  async searchBlog(query: BlogSearchQuery) {
+    const { title, content } = query;
+    if (title && content) {
+      const blogs = await this.blogRepo.find({
+        where: {
+          title: Like(`%${title}%`),
+        },
+      });
+      return blogs;
+    }
+  }
 }
--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Patch,
+  Delete,
+  Query,
+} from '@nestjs/common';
 
 import { CreateBlogDto } from './dtos/create-blog.dto';
 import { UpdateBlogDto } from './dtos/update-blog.dto';
 import { BlogService } from './Blog.service';
+import { BlogSearchQuery } from './dtos/blog-search-query.dto';
 
 @Controller('blog')
 export class BlogController {
   constructor(private blogService: BlogService) {}
+
+  @Get()
+  async getAllBlogs() {
+    return this.blogService.getAllBlogs();
+  }
 
   @Post('/create')
   async createBlog(@Body() body: CreateBlogDto) {
@@ -21,5 +36,16 @@ export class BlogController {
   @Patch('/:id')
   async updateblog(@Param('id') id: string, @Body() body: UpdateBlogDto) {
     return await this.blogService.updateBlog(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  async deleteBlog(@Param('id') id: string) {
+    return this.blogService.deleteBlog(parseInt(id));
+  }
+
+  @Get('/search/blog')
+  async search(@Query() query: BlogSearchQuery) {
+    console.log(query);
+    return await this.blogService.searchBlog(query);
   }
 }
